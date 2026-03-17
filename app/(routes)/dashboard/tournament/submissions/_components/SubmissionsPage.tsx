a694c4be2828a36723d5f1b951e77dd1cfa764a1
eliasvalleau-dev <elias.valle.au@gmail.com>
2026-03-17 21:05:22 +1100

    added routes to tournament and submissions

---
+++ app/(routes)/dashboard/tournament/submissions/_components/SubmissionsPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const SubmissionsPage = () => {
+  return (
+    <div className="text-4xl">
+      This is the dashboard page.
+    </div>
+  );
+};
+
+export default SubmissionsPage;

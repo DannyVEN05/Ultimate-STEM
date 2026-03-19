bd32979497bb2508dbb2185887a06af6870eafd6
Mridini <mridini.chonkar@gmail.com>
2026-03-19 13:13:52 +1100

    inital boxes structure

--- app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
+++ app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
@@ -5,13 +5,41 @@ import { useRouter } from "next/navigation";
 
 const OneVsOnePage = () => {
   const router = useRouter();
+
+ 
+
   return (
     <div className="text-4xl">
+      <h1 className="mb-8 text-4xl font-bold text-center">{'{tournamentName}'}</h1>
+      <div className="my-4 h-1 w-full bg-black" />
       <UsButton variant="red" onClick={() => {router.push("./")}}>
         Back
       </UsButton>
-      This is the one vs one page.
+
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-1 mt-8 justify-items-center">
+      {/* book 1 item */}
+      <div className="border rounded-lg p-4 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px]">
+        <h2 className="text-4xl font-semibold">Book title</h2>
+        <p className="text-xl text-gray-600 mt-2">Book description will go here.</p> 
+      
+        <UsButton sizeOptions={{height: 50, width: 100}} variant="green" textSize="text-lg" onClick={() => {router.push("/")}}>
+        Vote
+      </UsButton>
+      
+      </div>
+      {/* book 2 item */}
+      <div className="border rounded-lg p-4 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px]">
+        <h2 className="text-4xl font-semibold">Book title</h2>
+        <p className="text-xl text-gray-600 mt-2">Book description will go here.</p> 
+      
+        <UsButton sizeOptions={{height: 50, width: 100}} variant="green" textSize="text-lg" onClick={() => {router.push("/")}}>
+        Vote
+      </UsButton>
+      
+      </div>    
+      
     </div>
+  </div>
   );
 };
 

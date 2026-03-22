f0fadcaa569515ec5a1a9e65c2f6bdf25d8fc59f
Mridini <mridini.chonkar@gmail.com>
2026-03-22 21:09:32 +1100

    ui change

--- app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
+++ app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import UsButton from "@/app/_common/ui/buttons/UsButton";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 const OneVsOnePage = () => {
@@ -15,30 +16,45 @@ const OneVsOnePage = () => {
       <UsButton variant="red" onClick={() => {router.push("./")}}>
         Back
       </UsButton>
+      
+      <p className="mx-auto mt-3 max-w-4xl text-base font-bold text-gray-700 sm:text-2xl text-center">Vote for the best book in this round!</p>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-1 mt-8 justify-items-center">
+    <div className="grid grid-cols-1 md: grid-cols-[1fr_auto_1fr] gap-8 mt-8 items-center text-center justify-items-center">
+      
       {/* book 1 item */}
-      <div className="border rounded-lg p-4 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px]">
+      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px] flex flex-col">
         <h2 className="text-4xl font-semibold">Book title</h2>
-        <p className="text-xl text-gray-600 mt-2">Book description will go here.</p> 
-      
-        <UsButton sizeOptions={{height: 50, width: 100}} variant="green" textSize="text-lg" onClick={() => {router.push("/")}}>
+  
+        
+        <p className="text-xl text-gray-600 mt-6">Book description will go here.</p> 
+        
+        <div className="mt-auto flex justify-center">
+        <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-7 text-lg " style={{ cursor: "pointer" }} onClick={() => {router.push("/")}}>
         Vote
-      </UsButton>
-      
+      </Button>
       </div>
-      {/* book 2 item */}
-      <div className="border rounded-lg p-4 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px]">
-        <h2 className="text-4xl font-semibold">Book title</h2>
-        <p className="text-xl text-gray-600 mt-2">Book description will go here.</p> 
+      </div>
 
-        <div className="flex items-center justify-center flex-1">
-        <UsButton sizeOptions={{height: 50, width: 100}} variant="green" textSize="text-lg" onClick={() => {router.push("/")}}>
+
+      <div className="flex items-center justify-center">
+            <div className="rounded-full bg-slate-900 px-7 py-3 text-lg font-bold uppercase tracking-[0.2em] text-white shadow-lg">
+              VS
+            </div>
+        </div>
+
+      {/* book 2 item */}
+      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px] flex flex-col">
+        <h2 className="text-4xl font-semibold">Book title</h2>
+        <p className="text-xl text-gray-600 mt-6">Book description will go here.</p> 
+
+        <div className="mt-auto flex justify-center">
+        <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-7 text-lg " style={{ cursor: "pointer" }} onClick={() => {router.push("/")}}>
         Vote
-      </UsButton>
+      </Button>
       </div>    
      </div>
     
+   
     </div>
   </div>
   );

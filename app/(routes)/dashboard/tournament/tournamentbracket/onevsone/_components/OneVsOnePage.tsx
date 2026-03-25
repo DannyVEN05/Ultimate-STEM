1bda99d01708fdf32b5bc54ff689473943840e6a
Mridini <mridini.chonkar@gmail.com>
2026-03-25 22:03:27 +1100

    changes after feedback

--- app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
+++ app/(routes)/dashboard/tournament/tournamentbracket/onevsone/_components/OneVsOnePage.tsx
@@ -1,37 +1,67 @@
 "use client";
 
-import UsButton from "@/app/_common/ui/buttons/UsButton";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-
+import { useState } from "react";
 const OneVsOnePage = () => {
   const router = useRouter();
+  
 
+const [selectedBook, setSelectedBook] = useState<string | null>(null);
+
+  const book1 = {
+    title: "Book One",
+    author: "Author 1",
+    description: "Book one description goes here.",
+    cover: "https://m.media-amazon.com/images/I/71Z9DuY6lCL._AC_UF1000,1000_QL80_.jpg",
+  };
+
+  const book2 = {
+    title: "Book Two",
+    author: "Author 2",
+    description: "Book two description goes here.",
+    cover: "https://m.media-amazon.com/images/I/71Z9DuY6lCL._AC_UF1000,1000_QL80_.jpg",
+  };
  
+
+
+
 
   return (
     <div className="text-4xl">
       <h1 className="mb-8 text-4xl font-bold text-center">{'{tournamentName}'}</h1>
       <div className="my-4 h-1 w-full bg-black" />
-      <UsButton variant="red" onClick={() => {router.push("./")}}>
+      <Button onClick={() => {router.push("./")}}>
         Back
-      </UsButton>
+      </Button>
       
-      <p className="mx-auto mt-3 max-w-4xl text-base font-bold text-gray-700 sm:text-2xl text-center">Vote for the best book in this round!</p>
+      <p className="mx-auto mt-4 max-w-4xl text-base font-bold text-gray-700 sm:text-2xl text-center">Vote for the best book in this round!</p>
+      
 
-    <div className="grid grid-cols-1 md: grid-cols-[1fr_auto_1fr] gap-8 mt-8 items-center text-center justify-items-center">
+    <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-5 mt-5 items-center text-center justify-items-center">
       
       {/* book 1 item */}
-      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px] flex flex-col">
-        <h2 className="text-4xl font-semibold">Book title</h2>
-  
-        
-        <p className="text-xl text-gray-600 mt-6">Book description will go here.</p> 
+      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[700px] w-[500px] flex flex-col">
+        <img
+            src={book1.cover}
+            alt={book1.title}
+            className="w-95 h-100 object-cover rounded-md mx-auto shadow-md"
+        />
+      
+        {/* <h2 className="text-3xl font-semibold ">{book1.title}</h2> */}
+        <h2 className="text-xl text-gray-600 font-semibold text-left mt-3 mx-auto">By: {book1.author}</h2>
+        <p className="text-xl text-gray-600 mt-6">{book1.description}</p> 
         
         <div className="mt-auto flex justify-center">
-        <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-7 text-lg " style={{ cursor: "pointer" }} onClick={() => {router.push("/")}}>
-        Vote
+    
+    <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-5.5 text-lg" onClick={() => {
+      const confirmed = window.confirm(`Confirm your vote for ${book1.title}. This cannot be changed.`);
+      if (confirmed) {
+        setSelectedBook(book1.title)
+        router.push("./");}}}
+        > Vote 
       </Button>
+      
       </div>
       </div>
 
@@ -43,14 +73,27 @@ const OneVsOnePage = () => {
         </div>
 
       {/* book 2 item */}
-      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[600px] w-[500px] flex flex-col">
-        <h2 className="text-4xl font-semibold">Book title</h2>
-        <p className="text-xl text-gray-600 mt-6">Book description will go here.</p> 
+      <div className="border rounded-lg p-6 mt-8 bg-gray-100 shadow-md h-[700px] w-[500px] flex flex-col">
+        <img
+            src={book2.cover}
+            alt={book2.title}
+            className="w-95 h-100 object-cover rounded-md mx-auto shadow-md"
+          />
+
+        {/* <h2 className="text-4xl font-semibold">{book2.title}</h2> */}
+        <h2 className="text-xl text-gray-600 font-semibold text-left mt-3 mx-auto">By: {book2.author}</h2>
+        <p className="text-xl text-gray-600 mt-6">{book2.description}</p> 
 
         <div className="mt-auto flex justify-center">
-        <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-7 text-lg " style={{ cursor: "pointer" }} onClick={() => {router.push("/")}}>
-        Vote
+        
+      <Button className="bg-green-300 hover:bg-green-400 text-gray-700 px-10 py-5.5 text-lg" onClick={() => {
+        const confirmed = window.confirm(`Are you sure you want to vote for ${book2.title}? This cannot be changed.`);
+        if (confirmed) {
+          setSelectedBook(book2.title)
+          router.push("./");}}}
+        > Vote 
       </Button>
+
       </div>    
      </div>
     

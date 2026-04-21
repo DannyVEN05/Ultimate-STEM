5ca985fc26a24bee5de49b2f6991f176592fad2a
DannyVEN05 <danny.venizelou@gmail.com>
2026-04-21 13:11:29 +1000

    Profile page UI update

--- app/(routes)/(auth)/profile/_components/ProfilePage.tsx
+++ app/(routes)/(auth)/profile/_components/ProfilePage.tsx
@@ -10,14 +10,16 @@ const ProfilePage: React.FC = () => {
 
   const BookCard = () => {
     return (
-      <div className="relative flex h-full w-full bg-secondary/30 shadow-md border border-gray-200 rounded-lg p-4 gap-4 hover:shadow-xl transition-all hover:bg-secondary/50">
+      <div className="relative flex h-full w-[100%] shadow-md border border-gray-200 rounded-lg p-4 gap-4 hover:shadow-xl transition-all hover:bg-secondary/30">
         <div className="absolute top-2 right-2">
           <Button variant="outline">
             Edit
           </Button>
         </div>
-        <div className="flex-1 bg-white shadow-md border border-gray-200 rounded-lg hover:shadow-xl transform transition hover:-translate-y-1"></div>
-        <div className="flex-1 flex flex-col gap-2">
+        <div className="flex-[4] bg-white shadow-md border border-gray-200 rounded-lg hover:shadow-xl transform transition hover:-translate-y-1">
+          <img src="/covers/engineering.png" alt="Book Cover" className="h-full w-full object-cover rounded-lg" style={{ width: "100%", height: "100%" }} />
+        </div>
+        <div className="flex-[6] flex flex-col gap-2">
           <h3 className="text-xl font-bold">{`{Book Title}`}</h3>
           <p className="text-md text-muted-foreground"><strong>Genre:</strong> Fiction</p>
           <p className="text-md text-muted-foreground -mb-2"><strong>Description:</strong></p>
@@ -30,23 +32,23 @@ const ProfilePage: React.FC = () => {
   }
 
   return (
-    <div className="px-4 h-full flex flex-col min-h-0">
+    <div className="mx-36 h-full flex flex-col min-h-0">
 
-      <div className="flex gap-4 justify-center items-center mb-2">
+      <div className="flex gap-38 justify-center items-center mb-2">
         <div className="flex-1">
-          <h1 className="text-3xl font-bold text-center">My Profile</h1>
+          <h1 className="text-3xl font-bold text-center pl-4 pr-1">My Profile</h1>
         </div>
         <div className="relative flex-[3]">
-          <h1 className="text-3xl font-bold text-center">My Submissions</h1>
-          <div className="flex absolute top-1/2 -translate-y-1/2 right-0 justify-center items-center">
+          <h1 className="text-3xl font-bold text-center px-5">My Submissions</h1>
+          <div className="flex absolute top-1/2 -translate-y-1/2 right-4 justify-center items-center">
             <Button variant="secondary" className="">Submit a new idea</Button>
           </div>
         </div>
       </div>
 
-      <div className="flex gap-4 h-full min-h-0">
-        <div className="flex flex-col flex-1">
-          <div className="flex-[5] m-2 border border-gray-200 shadow-xs rounded-xl justify-center items-center flex flex-col p-4 bg-gradient-to-b from-secondary/50 to-white hover:from-secondary/70 hover:to-secondary/10 transition-colors hover:shadow-md">
+      <div className="flex gap-38 h-full min-h-0">
+        <div className="flex flex-col flex-1 pl-2">
+          <div className="flex-[5] my-2 mr-2 ml-2 border border-gray-200 shadow-xs rounded-xl justify-center items-center flex flex-col p-4 bg-gradient-to-b from-secondary/50 to-white hover:from-secondary/70 hover:to-secondary/10 transition-colors hover:shadow-md">
             <Avatar className="h-32 w-32 shadow-md hover:shadow-lg transition-shadow">
               <AvatarImage alt="@username" />
               <AvatarFallback style={{ backgroundColor: "white", fontSize: 24 }}>{user?.user_firstname?.charAt(0)}{user?.user_lastname?.charAt(0)}</AvatarFallback>
@@ -70,7 +72,7 @@ const ProfilePage: React.FC = () => {
           </div>
         </div>
         <div className="flex-[3] flex flex-col items-center gap-2 min-h-0">
-          <div className={`h-full w-full rounded-xl border border-gray-100 shadow-[inset_0_3px_10px_0_rgba(0,0,0,0.5)] grid grid-cols-6 auto-rows-[60%] gap-4 m-2 p-8 overflow-y-auto`}>
+          <div className={`h-full w-full rounded-lg px-4 grid grid-cols-6 auto-rows-[30%] gap-12 py-2 overflow-y-auto`}>
             <div className={`col-span-3`}><BookCard /></div>
             <div className={`col-span-3`}><BookCard /></div>
             <div className={`col-span-3`}><BookCard /></div>

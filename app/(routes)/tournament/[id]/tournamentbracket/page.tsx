7a86e3391b824ba39e675779ffbe636137e6fefd
Enerel <enerelts0011@gmail.com>
2026-05-07 12:43:20 +1000

    tournament small bugs fixed, concept submissions small bugs fixed, cron jobs created

--- app/(routes)/tournament/[id]/tournamentbracket/page.tsx
+++ app/(routes)/tournament/[id]/tournamentbracket/page.tsx
@@ -6,8 +6,9 @@ export const metadata: Metadata = {
   description: "Ultimate STEM",
 };
 
-const TournamentBracket: NextPage = () => {
-  return <TournamentBracketPage />;
+const TournamentBracket = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  return <TournamentBracketPage id={id} />;
 };
 
 export default TournamentBracket;
